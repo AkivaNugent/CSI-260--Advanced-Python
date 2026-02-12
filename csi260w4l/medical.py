2cb099f1f5bb86329c8fbb1328b489642e59af6b
AkivaNugent <AkivaNugent@gmail.com>
2026-02-12 16:35:51 -0500

    :)

---
+++ csi260w4l/medical.py
@@ -0,0 +1,11 @@
+class Patient:
+    def __init__(self, first_name, last_name, phone_number, ec_first_name, ec_last_name, ec_phone_number):
+        first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.ec_first_name = ec_first_name
+        self.ec_last_name = ec_last_name
+        self.ec_phone_number = ec_phone_number
+
+class Procedure:
+    def __init__(self):
